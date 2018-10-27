9d408c160f96f155089291700e5403e01216c4b5
Samuel Wilson <samuel.wilson@fluxfederation.com>
2018-10-27 19:54:45 +1300

    Implement simple single digit addition

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,20 @@
-use std::str::FromStr;
-
 fn main() {
     println!("Hello, world!");
 }
 
-const PLUS: char = '+';
+fn calculate(expression: String) -> u32 {
+    let mut terms = expression.chars();
+    let first_operand = terms.next().unwrap().to_digit(10).unwrap();
+    let operator = terms.next().unwrap();
+    let second_operand = terms.next().unwrap().to_digit(10).unwrap();
 
-enum Token<'a> {
-    Number(usize),
-    Operator(&'a char)
-}
-
-struct Expression<'a> {
-    first_operand: Token<'a>, 
-    operator: Token<'a>,
-    second_operand: Token<'a>
-}
-
-fn calculate(input: String) -> usize {
-    let expression = tokenize(input);
-    let value = interpret(expression);
-    value
-}
-
-fn tokenize<'a>(input: String) -> Expression<'a> {
-    let mut characters = input.chars().peekable();
-    let mut tokens = Vec::new();
-
-    while let Some(_) = characters.peek() {
-        match characters.peek() {
-            Some('0'...'9') => tokens.push(number_token(characters.next())),
-            Some(&PLUS) => tokens.push(operator_token(characters.next())),
-            _ => panic!("Characters must be numbers or operators."),
-        }
-    }
-
-    // Build expression from tokens using pattern matching
-    Expression(Token::Number(5), Token::Operator(&PLUS), Token::Number(3))
-}
-
-fn number_token<'a>(literal: Option<char>) -> Token<'a> {
-    let value = literal.expect("Shomehow this is dead wrong.").to_digit(10).unwrap() as usize;
-    Token::Number(value)
-}
-
-fn operator_token<'a>(literal: Option<char>) -> Token<'a> {
-    if let Some(PLUS) = literal {
-        Token::Operator(&PLUS)
-    } else {
-        panic!("Unkown operator.");
+    match operator {
+        '+' => add(first_operand, second_operand),
+        _ => panic!("Unsupported operation.")
     }
 }
 
-fn interpret(expression: Expression) -> usize {
-    match expression {
-        Expression(Token::Number(first_operand), Token::Operator(&PLUS), Token::Number(second_operand)) => add(first_operand, second_operand),
-        _ => panic!("Expression can't be interpreted.")
-    }
-}
-
-fn add(first_operand: usize, second_operand: usize) -> usize {
+fn add(first_operand:u32, second_operand: u32) -> u32 {
     first_operand + second_operand
 }
 
