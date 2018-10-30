40d6cbad5cdf94fc9cabaf30b3b62fe8531846aa
Samuel Wilson <samuel.wilson@fluxfederation.com>
2018-10-30 17:43:04 +1300

    Handle invalid whitespace input

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,31 @@ fn main() {
     repl();
 }
 
+fn repl() {
+    let scanner = io::stdin();
+    let mut input = String::new();
+
+    loop {
+        match scanner.read_line(&mut input) {
+            Ok(_) => {
+                if is_invalid(&input) {
+                    input = "".to_string(); // Clear the buffer.
+                    continue;
+                }
+
+                print_result(calculate(&input));
+            },
+            Err(error) => println!("Error scanning input: {}", error)
+        }
+
+        input = "".to_string(); // Clear the buffer.
+    }
+}
+
+fn is_invalid(input: &String) -> bool {
+    input.chars().all(|c| c.is_whitespace())
+}
+
 fn calculate(expression: &String) -> Result<u32, String> {
     let mut terms = expression.chars();
     let first_operand = terms.next().unwrap().to_digit(10).unwrap();
@@ -21,27 +46,22 @@ fn add(first_operand:u32, second_operand: u32) -> u32 {
     first_operand + second_operand
 }
 
-fn repl() {
-    let scanner = io::stdin();
-    let mut input = String::new();
-
-    loop {
-        match scanner.read_line(&mut input) {
-            Ok(_) => {
-                println!("You entered: {:?}", &input);
-                print_result(calculate(&input));
-            },
-            Err(error) => println!("Error scanning input: {}", error)
-        }
-        input = "".to_string(); // Clear the buffer.
-    }
-}
-
 fn print_result(result: Result<u32, String>) {
     match result {
         Ok(value) => println!("{}", value),
         Err(err) => println!("{}", err)
     }
+}
+
+#[test]
+fn test_is_invalid() {
+    let empty_line = "\n".to_string();
+    let empty_string = "".to_string();
+    let whitespace = "\n     \t\t  \n".to_string();
+
+    assert!(is_invalid(&empty_line));
+    assert!(is_invalid(&empty_string));
+    assert!(is_invalid(&whitespace));
 }
 
 #[test]
@@ -55,4 +75,5 @@ fn test_calculate() {
     assert_eq!(calculate(&addition1).unwrap(), 8, "Addition of 5 + 3");
     assert_eq!(calculate(&addition2).unwrap(), 3, "Addition of 1 + 2");
 }
+
 
